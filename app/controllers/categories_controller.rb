185703bf251195646a18ecfbaf7671ea5974d643
sid314 <sidaa314@gmail.com>
2026-01-05 20:05:04 +0530

    formatting

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,42 +1,42 @@
 class CategoriesController < ApplicationController
   def index
-  @categories = Category.all
+    @categories = Category.all
   end
   def show
-  @category = Category.find(params[:id])
-  @items = @category.items.order(ranking: :desc)
+    @category = Category.find(params[:id])
+    @items = @category.items.order(ranking: :desc)
   end
   def new
-  @category = Category.new
+    @category = Category.new
   end
   def create
-  @category = Category.new(category_params)
-  if @category.save
-    flash[:notice] = "Category created successfully"
-    redirect_to category_path(@category)
-  else
-    render :new, status: :unprocessable_entity
-  end
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category created successfully"
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   def edit
-  @category = Category.find(params[:id])
+    @category = Category.find(params[:id])
   end
   def update
-  @category = Category.find(params[:id])
-  if @category.update(category_params)
-    flash[:notice] = "Category updated successfully"
-    redirect_to category_path(@category)
-  else
-    render :edit, status: :unprocessable_entity
-  end
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category updated successfully"
+      redirect_to category_path(@category)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
   def destroy
-  @category = Category.find(params[:id])
-  @category.destroy
-  flash[:notice] = "Category deleted successfully"
-  redirect_to categories_path
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:notice] = "Category deleted successfully"
+    redirect_to categories_path
   end
   def category_params
-  params.require(:category).permit(:name)
+    params.require(:category).permit(:name)
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -58,13 +58,13 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params.expect(:id))
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find(params.expect(:id))
+  end
 
-    # Only allow a list of trusted parameters through.
-    def item_params
-      params.expect(item: [ :name, :ranking, :category_id ])
-    end
+  # Only allow a list of trusted parameters through.
+  def item_params
+    params.expect(item: [ :name, :ranking, :category_id ])
+  end
 end
